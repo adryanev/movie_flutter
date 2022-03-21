6296624b6eaa4f806f7cbd96b9ebcfb7c48ed5a5
Adryan Eka Vandra <adryanekavandra@gmail.com>
2022-03-21 20:14:27 +0700

    ignore one member abstract for base usecase

--- lib/core/domain/use_case.dart
+++ lib/core/domain/use_case.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: one_member_abstracts
+
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
 import 'package:movie_flutter/core/domain/failures/failure.dart';
